a8ee5c4c113ff5be8ad6e42234391e2dafd82994
omarelrefaey <omarrefaey1@gmail.com>
2020-12-09 21:20:16 +0200

    test

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main()
+{
+    char temp;
+    temp = 50;
+    printf("temp = %d",temp);
+    return 0;
+}
+*/
+/*int main()
+{
+    int x = 20;
+    printf("%d",x);
+    return 0;
+}
+*/
+/*int main()
+{
+    int x = 20;
+    printf("%d",30);
+    return 0;
+}*/
+
+/*int main()
+{
+    int x = 20;
+    int y = 50;
+    printf("%d\n",x);
+    printf("%d",y);
+    return 0;
+}*/
+
+/*int main()
+{
+    int x = 20;
+    int y = 50;
+    printf("%d",x);      // try spaces then \t then \n
+    printf("%d",y);
+    return 0;
+}*/
+
+/*int main()
+{
+    int x = 20;
+    int y = 50;
+    printf("%d      %d",x,y);
+    return 0;
+}*/
+
+/*int main()
+{
+    int x = 20;
+    int y = 50;
+    printf("x = %d\ny = %d",x,y);
+    return 0;
+}*/
+
+/*int main()
+{
+    int temp = 20;
+    printf("temp = %d",temp);
+    return 0;
+}*/
+
+int main()
+{
+    int x = 20;
+    int y = 1;
+    printf("x = %d\ny = %2d",x,y);    // try %2d in y and see diff.
+    return 0;
+}
